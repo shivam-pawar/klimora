87c37b409312c4d5f5cdbf1d88fbe06dbd3c9671
shivam-pawar <shivampawar1038@gmail.com>
2024-12-22 16:22:58 +0530

    update GeoCodeBaseUrl to use HTTPS

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
   baseUrl: "https://api.openweathermap.org/data/2.5",
-  GeoCodeBaseUrl: "http://api.openweathermap.org/geo/1.0",
+  GeoCodeBaseUrl: "https://api.openweathermap.org/geo/1.0",
   API_KEY: import.meta.env.VITE_OPENWEATHER_API_KEY,
   DEFAULT_PARAMS: {
     units: "metric",
